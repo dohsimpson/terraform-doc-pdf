e111e262c07f354ab87f4e8161c57a5f7ab6a496
dohsimpson <mydohsimpson@gmail.com>
2018-04-02 16:10:32 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Terraform PDF Documentation
+
+### This repository contains all the Terraform documentation in PDF format, generated automatically.
+
+## Dependencies
+* `xelatex`: I installed it on my Macbook by: 1. Install [pkg](http://www.texts.io/support/0001/) 2. Download [ucharcat.sty](http://www.tug.org/texlive//devsrc/Master/texmf-dist/tex/latex/ucharcat/ucharcat.sty) 3. Place it under a certain [directory](https://tex.stackexchange.com/questions/121257/how-do-i-install-a-style-file-on-mac-10-8-other-answers-dont-seem-to-work#comment270807_121257)
+* `pandoc`: For mac, `brew install pandoc`
+* `python3`
+    * `pipenv`
+    * `pypandoc`
+    * `requests-html`
+
+# How to run the code
+1. Install the dependencies. Use `$ pipenv install` for installing python packages.
+2. `$ pipenv shell`
+3. `$ python terraform-doc.py`
---
+++ terraform-doc.py
@@ -0,0 +1,36 @@
+import requests_html as rh
+import pypandoc
+
+def generate_provider_pdf(url, filename, s=None):
+    s = rh.HTMLSession() if not s else s
+    r1 = s.get(url)
+
+    html = ""
+    anchors = r1.html.find('.nav-visible a')
+    links = [a.absolute_links.pop() for a in anchors]
+    links = filter(lambda href: href.find('/r/') != -1 or href.find('/d/') != -1, links) # filter out links not data or resource
+
+    print("downloading...")
+    for l1 in links:
+        r2 = s.get(l1)
+        # r2.html.render()
+        div = r2.html.find('#inner', first=True)
+        # with open("/tmp/b.html", "wt") as f:
+        #     f.write(content.html)
+        if div:
+            html += div.html
+
+    print("generating pdf...")
+    try:
+        output = pypandoc.convert_text(html, "pdf", format="html", outputfile="./{}.pdf".format(filename), extra_args=['--pdf-engine=xelatex'])
+    except Exception as e:
+        print(e)
+
+if __name__ == '__main__':
+    s = rh.HTMLSession()
+    r = s.get("https://www.terraform.io/docs/providers/index.html")
+    providers = r.html.find('.table a')
+    provider_pairs = [(p.absolute_links.pop(), p.text) for p in providers]
+    for url, filename in provider_pairs:
+        print(filename)
+        generate_provider_pdf(url, filename, s=s)
